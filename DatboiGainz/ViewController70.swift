24252e2ac73f27b4a2148e7980c0174e1759dbdc
Ruben rojo <61779@psdschools.org>
2021-10-21 11:50:28 -0600

    bicep and shoulder

---
+++ DatboiGainz/ViewController70.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController70.swift
+//  DatboiGainz
+//
+//  Created by Ruben Rojo on 10/21/21.
+//
+
+import UIKit
+
+class ViewController70: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
